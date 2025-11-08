84e34755c12b8ecd7ddb67eeaffacc4682b66b98
Aqeel <aqeelvahmed@gmail.com>
2025-11-08 17:27:41 +0000

    Update ebayScraper.py

--- ebayScraper.py
+++ ebayScraper.py
@@ -32,22 +32,19 @@ class Scraper:
         self.userID = id
         self.userItem = item
 
-    
-    def driver_configure(self):
+    def driver_configure(self):  # <--- Driver configured on call to ensure driver independence
         options = Options()
         options.add_argument('--headless')
-        service = Service(executable_path='') # -- crontab requires geckodriver path
+        service = Service(executable_path='/usr/local/bin//geckodriver')
         return options, service    
-
     
     def total_price(self, x):
-        """Retrieves both Item & Postage Price (by £) and returns total"""
+        """Retrieves Item/Postage Price and returns total"""
         prices = [float(i) for i in re.findall(r'£(\d+\.+\d*)', x)]
         return np.round(sum(prices), 2)
-
     
     def convert_into_hours(self, x):
-        """If exist, return time left in minutes"""
+        """If exists, returns time left in minutes"""
         minutes = 0
         try:
             x = x.split('\n')
@@ -64,41 +61,52 @@ class Scraper:
         except ValueError:  # Only interested where Time left exists
             return None
 
-    
+
+
+
+ 
     def item_Scraper(self, glance, dbIngest):
-        """Scrape item metadata"""
+        """Scrape Item and retrieve Results, Metadata and Links"""
         try:
             o, s = self.driver_configure()
             with webdriver.Firefox(options=o, service=s) as driver:
+                # --- Use driver to search item
                 driver.get('https://www.ebay.co.uk/sch/i.html?_nkw={}'.format(self.item))
-                time.sleep(3)
-                logging.info(f'{self.item} succesfully searched')
+                time.sleep(5)
+                logging.info(f'Succesfully searched: {self.item}')
+
+                # --- Retrieve number of relevant results
                 nums = (driver.find_elements(By.CLASS_NAME, 'srp-controls__count-heading'))[0].text
-                logging.info(f"""nums is {nums}""")    
-                self.numResults = int("".join(re.findall(r'\d+', nums)))  # -- Number of relevant results (Stored in Class)  
-                data = [s.text for s in driver.find_elements(By.CLASS_NAME, 's-item')]  # Item details
-                links = [s.get_attribute('href') for s in driver.find_elements(By.CLASS_NAME, 's-item__link')]  # Item hyperlinks
-                logging.info(f'{self.item} metadata retrieved')
-                if glance:
-                    logging.info(f'{self.item} metadata manipulation beginning ...')
-                    self.item_manipulation(data, links, dbIngest)  # --- Allows for scraperCall without adding to            
-        except Exception as e:
-            logging.error(f'Scrape Error has Ocurred: {e}')
-            bot.send_message(self.userID, f'Sorry an Error has ocurred whilst Scraping: {e}')
+                self.numResults = int("".join(re.findall(r'\d+', nums)))
+
+                # --- Retrieve Metadata/Links information.
+                data = [s.text for s in driver.find_elements(By.CLASS_NAME, 'su-card-container__content')]  # Item details
+                links = [s.get_attribute('href') for s in driver.find_elements(By.CLASS_NAME, 's-card__link')][::2]  # Item hyperlinks
+                logging.info(f'Metadata Retrieved [Results: {self.numResults} | Data: {len(data)} | Links: {len(links)}]')
+
+                if glance:  # --- glance allows for Quick Top 3 without adding to database
+                    time.sleep(5)
+                    self.item_manipulation(data, links, dbIngest)      
+        except Exception:
+            if not dbIngest:  # -- Don't send during crontab run
+                bot.send_message(self.userID, f'Sorry an Error has ocurred whilst Scraping. Please Try Again!')
+            logging.exception(f'Scrape Error has Ocurred')
         finally:
             self.END = time.time() - self.START
-            subprocess.run(['pkill', '-f', 'geckodriver'])
+            subprocess.run(['pkill', '-f', 'geckodriver'])  # < --- Ensures driver independence
             subprocess.run(['pkill', '-f', 'firefox'])
         return self
-        
-        
-    def item_manipulation(self, data, links, dbIngest):
-        """Clean retreived data"""
 
+
+
+
+ 
+    def item_manipulation(self, data, links, dbIngest):
+        """Clean and Manipulate Search Results"""
         try:
             df = pd.DataFrame({'Metadata':data, 'Hyperlink':links})
             df['Type'] = np.select([df['Metadata'].str.lower().str.contains('buy it now'), df['Metadata'].str.lower().str.contains('time left'), df['Metadata'].str.lower().str.contains('best offer')], ['buy it now', 'auction', 'buy it now'], None)
-            df['Item_ID'] = df['Hyperlink'].apply(lambda x: int(re.findall(r'itm/(\d+)', x)[0]) if x else None)
+            df['Item_ID'] = df['Hyperlink'].str.extract(r'itm/(\d+)').astype('Int64').astype('object').where(lambda s: pd.notnull(s), None)
             df['Name'] = df['Metadata'].apply(lambda x: x.split('\n')[0] if x else None)
             df['Minutes'] = df['Metadata'].apply(self.convert_into_hours)
             df['Price'] = df['Metadata'].apply(self.total_price)
@@ -106,22 +114,23 @@ class Scraper:
             df['Status'] = 1
             df['userID'] = self.userID
             df['userItem'] = self.userItem
+            df.dropna(subset=['Metadata', 'Hyperlink', 'Item_ID', 'Name', 'shortLink'], inplace=True)  
+            self.df = df[2:(2+self.numResults)] if self.numResults < len(df) else df[2:]  # If results<df, remove extra as likely irrelevant
+            logging.info(f'Metadata Manipulation Complete')
             
-            if self.numResults < len(df):  # First two always seem to be irrelevant
-                df = df[2:(2+self.numResults)]
-            else:
-                df = df[2:]
-            self.df = df
+            if dbIngest:  # -- dbIngest allows for results ingestion into database
+               self.ingestion()
+        except Exception:
+            if not dbIngest:  # -- Don't send during crontab run
+                bot.send_message(self.userID, f'Sorry an Error has ocurred during Preperation. Please Try Again!')
+            logging.exception(f'Manipulation Error has Ocurred')
+        finally:
             self.END = time.time() - self.START
-            logging.info(f'{self.item} metadata manipulation complete')
-            if dbIngest:
-                logging.info(f'{self.item} database ingestion beginning ...')
-                self.ingestion()
-        except Exception as e:
-            logging.error('Manipulation error has occurred: {e}')
-            bot.send_message(self.userID, f'Sorry an Error has ocurred during Manipulation: {e}')
 
-    
+
+
+
+ 
     def ingestion(self):
         try:
             cur.execute("UPDATE TRACKED_LIST SET Status=0 WHERE userID=? AND userItem=?", (self.userID, self.userItem))
@@ -129,21 +138,77 @@ class Scraper:
             toIngest = [tuple(row) for row in self.df[['userID','userItem','Name','Item_ID','Type','Minutes','Price','Hyperlink','shortLink','Metadata','Status']].values]
             cur.executemany("INSERT OR REPLACE INTO TRACKED_LIST (userID, userItem, Name, Item_ID, Type, Minutes, Price, Hyperlink, shortLink, Metadata, Status) VALUES (?,?,?,?,?,?,?,?,?,?,?)", toIngest)
             db.commit()  # --- Batch insert all newlyTracked
-        except Exception as e:
-            logging.error(f'Ingestion Error has Ocurred: {e}')
-            bot.send_message(self.userID, f'Sorry an Error has occured whilst Scraping: {e}')
-        
-        logging.info('Data Ingested')        
+            logging.info(f'Data Ingestion complete')
+
+        except Exception:
+            logging.exception(f'Ingestion Error has Ocurred')
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+##### -------------------------------------------------- CRONJOB LOGIC (Run this File at Specified Times) -------------------------------------------------- #####
+
+def toSend_formatter(toSend):
+    """Special characters must be backslashed to be sent"""
+    toSend = str(toSend)
+    for i in "\_*[],()~>#+-_=|!.'":
+        toSend = toSend.replace(i, f"\\{i}")
+    return toSend
 
 
 
 
 
 if __name__ == "__main__":
-    s = Scraper('apple pencil 2')
-    s.item_Scraper(False, False)
+    def itemScraper(item):
+        scraper = Scraper(item[1], item[0])
+        scraper.item_Scraper(glance=True, dbIngest=True)
+        df = scraper.df
+        candidates = df[((df.Price < int(item[2])) & (df.Price >= int(item[2])*0.5)) & ((df.Type == 'Auction') & (df.Minutes < 600) | (df.Type != 'Auction'))].sort_values('Type', ascending=False)[:3]
+        return candidates  # --- Candidate Logic: <£tgt and >0.5*£tgt, <600mins for Auction
+    
+    # --- toScrape_IDs returned as ID: [All Applicable Items]
+    response = sys.argv[1]
+    vals, toScrape_IDs = {'Type1':[3], 'Type2':[0,1,2], 'Type3':[3], 'Type4':[2], 'Type5':[3]}.get(response), {}
+    toScrape = cur.execute(f"SELECT * FROM TRACKED_ITEMS WHERE Frequency IN ({','.join('?'*len(vals))}) ORDER BY ID", vals).fetchall()
+    for row in toScrape:
+        if row[0] in toScrape_IDs.keys():
+            toScrape_IDs[row[0]].append(row)
+        else:
+            toScrape_IDs[row[0]] = [row]
+
+    # --- Loop through all user Items then send together
+    for userID, userItems in toScrape_IDs.items():
+        allToSend = []
+        for item in userItems:
+            message = f"Matches for {item[1].replace('-', ' ')} \[Target: £{toSend_formatter(item[2])}\]\n\n"
+            try:
+                matched = itemScraper(item)
+                if len(matched) > 0:
+                    for i, row in matched.iterrows():
+                        toSend = '\- {} Item found for [£{}]({})\n\n'.format((row.Type).capitalize(), toSend_formatter(row.Price), toSend_formatter(row.shortLink)) if row.Type != 'Auction' \
+                        else '\- Auction Item found with {} minutes left is currently [£{}]({})\n\n'.format(toSend_formatter(int(row.Minutes)), toSend_formatter(row.Price), toSend_formatter(row.shortLink))
+                        message += toSend
+                else:
+                    message = f"😔 No Satisfactory matches were found for '{item[1]}'!"
+            except Exception as e:
+                logging.exception(f'Error ocurred during Send Off Call')
+                message = f"😔 An Error Ocurred whilst matching for '{item[1]}'!"
+            allToSend.append(message)
+        
+        for i in allToSend:
+            bot.send_message(userID, i, parse_mode='MarkdownV2')
